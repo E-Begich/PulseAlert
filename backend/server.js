18fdbb19e63e4b7b425edd515862ff0e168035c6
Emina <begic.emina92@gmail.com>
2026-01-14 12:10:15 +0100

    instalation and updates

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test ruta
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+// Pokretanje servera
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
